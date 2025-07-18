b3025e95f54ff2080ffc4493a3c0cee5324535ac
xxy-08 <873221675@qq.com>
2025-07-18 16:36:59 +0800

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -13,16 +14,20 @@ console.log(process.env.MONGO_URI);
 
 const app=express();
 const PORT=process.env.PORT || 5001;
+const __dirname = path.resolve(); 
 
 
 
 // Middleware
+if(process.env.NODE_ENV !== "production") {
+
 app.use(
     cors({
     origin:"http://localhost:5173", 
    
 })
 );
+}
 app.use(express.json()); 
 app.use(rateLimiter);
 // app.use((req,res,next)=>{
@@ -30,6 +35,15 @@ app.use(rateLimiter);
 //     next();
 // });
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+})
+}
+
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
